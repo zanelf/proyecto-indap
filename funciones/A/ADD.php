f2557002b4b757e2620e4d152853fd83da0ea376
zanelf_XD <z4n.3lf@gmail.com>
2022-12-05 11:39:26 -0300

    0.0.2b
    version inestable aun revisando correcciones y cambios

--- funciones/A/ADD.php
+++ funciones/A/ADD.php
@@ -1,4 +1,6 @@
 <?php
+
+
 $clave = "mane"; //la clave del usuario
 
 $conexion = pg_connect("host= "localhost" dbname= "INDAP" port= "5432"
@@ -8,13 +10,10 @@ $rut = $_POST["rut"];
 $nombre = $_POST["nombre"];
 $id_indap = $_POST["id_indap"];
 
-$sql = "insert into personal 
-values ('.$id_indap',' .$nombre', .$rut);";
+$sql = "insert into personal
+values ('.$id_indap.',' .$nombre.', .$rut.);";
 
 $insercion = pg_query($conexion,$sql);
 
-if (!$insercion) { //si no ha ocurrido un error
-    echo "<br><br>Miembro guardado con <b>éxito</b>"; 
-}
 
 ?>
--- funciones/A/GET.php
+++ funciones/A/GET.php
@@ -10,8 +10,12 @@ $sql = "select nombre, id_indap from personal  where rut = .$rut.;";
 
 $insercion = pg_query($conexion,$sql);
 
+var_dump($insercion);
 if (!$insercion) { //si no ha ocurrido un error
     echo "<br><br>Miembro guardado con <b>éxito</b>"; 
+}else{
+    echo "no entro";
+
 }
 
 ?>
