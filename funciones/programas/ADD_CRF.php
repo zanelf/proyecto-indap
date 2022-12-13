6fdd6188664bf76f6a163c1474d6c0aadd74a1a2
zanelf_XD <z4n.3lf@gmail.com>
2022-12-13 13:07:42 -0300

    0.6.1

---
+++ funciones/programas/ADD_CRF.php
@@ -0,0 +1,28 @@
+<?php
+include  "../../conexion.php";  //base para agarrar la conexion general
+
+$rut = $_POST["rut"];
+$nombre = $_POST["nombre"]; 
+$correo = $_POST["correo"];
+
+
+$sql = "INSERT INTO public.personal (nombre,rut)
+VALUES ('$nombre',$rut);
+
+insert into  public.trabajador (rut,correo_electronico)
+values ($rut,'$correo');
+
+insert into public.consultores_de_riesgo_y_fomento (rut)
+values ($rut);
+";
+
+$insercion = pg_query($coneccion,$sql);
+
+if($insercion){
+   echo "bienvenido a indap, esperamos poder seguir trabajando con usted";
+}else{
+    echo "los valores que ah ingresado ya existen o son incorrectos en la base de datos en la base de datos";
+}
+echo "<br><br><a href='../../index.html'>volver a la pagina anterior</a>";
+
+?>
