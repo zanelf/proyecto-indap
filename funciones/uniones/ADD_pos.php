f32d83f50988394915f354b13e376d32209e9580
zanelf_XD <z4n.3lf@gmail.com>
2022-12-16 10:40:08 -0300

    0.7.6

---
+++ funciones/uniones/ADD_pos.php
@@ -0,0 +1,27 @@
+<?php
+include  "../../conexion.php";  //base para agarrar la conexion general
+
+$id = $_POST["id"];
+$codigo = $_POST["codigo"];
+$ayuda = $_POST["ayuda"];
+
+$sql = "DELETE FROM public.especialidades
+        WHERE rol='$rol';"
+
+$insercion = pg_query($coneccion,$sql);
+
+echo "<br>";
+
+if($insercion){
+    if(pg_num_rows($insercion) == 0){
+        echo "ingrese un rut que exista";
+    }else{
+        echo "se ah cambiado el correo con exito";
+    }
+}else{
+    echo "el proceso a fallado, por favor ingrese un parametro valido";
+}
+
+echo "<br><br><a href='../../index.html'>volver a la pagina de inicio</a>";
+
+?>
