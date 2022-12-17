8d83784cdf02ece03cf10682a514bf9acdc58a0d
zanelf_XD <z4n.3lf@gmail.com>
2022-12-16 23:01:29 -0300

    0.9

--- funciones/consultas/GET_crf.php
+++ funciones/consultas/GET_crf.php
@@ -3,11 +3,11 @@ include  "../../conexion.php";  //base para agarrar la conexion general
 
 $rut = $_POST["rut"];
 
-$sql = "SELECT p.*, t.correo_electronico
+$sql = "SELECT p.nombre , t.correo_electronico
 from personal p, trabajador t, consultores_de_riesgo_y_fomento crf
 where p.rut = $rut
 and p.rut = t.rut  
-and t.rut = crf.rut,";
+and t.rut = crf.rut";
 
 $insercion = pg_query($coneccion,$sql);
 
@@ -21,12 +21,20 @@ echo "<br>";
         echo $ro["nombre"];
         echo ": <br><br> Correo electronico: ";
         echo $ro["correo_electronico"];
-        echo "<br><hr><br>especialidades";
-        
-        $sql = "
-        
-        ";
-    
+        echo "<br><hr><br>especialidades:<br>";
+
+        $second = pg_query($coneccion, "SELECT distinct e.nombre 
+                                         from especialidades e, tiene ti  
+                                         where ti.rut = $rut
+                                         and ti.id_especialidad = e.id_especialidades;
+        ");
+        echo "<br>";
+        while($row = pg_fetch_row($second)){
+            echo "> $row[0]";
+            echo "<br><br>"; 
+        }
+
+
     }
 
 echo "<br><br><a href='../../index.html'>volver a la pagina de inicio</a>";
--- funciones/establecimiento/GET_E.php
+++ funciones/establecimiento/GET_E.php
@@ -1,7 +1,7 @@
 <?php
 include  "../../conexion.php";  //base para agarrar la conexion general
 
-$rut = $_POST["rol"];
+$rol = $_POST["rol"];
 
 $sql = "SELECT direccion
 from establecimiento
@@ -11,12 +11,13 @@ $insercion = pg_query($coneccion,$sql);
     // pg_num_rows($insercion) == 0 
     if (!$insercion) { //verifica si el valor existe
         echo "Por favor ingrese un rol valido <br><br>";
-        echo "<a href='../../index.html'>volver a la pagina anterior</a>";
     }else{
         $ro = pg_fetch_array($insercion);
         echo "El establecimiento en la direccion ";
         echo $ro["direccion"];
         echo " Es propiedad de la plataforma INDAP";
     }
+
+    echo "<a href='../../index.html'>volver a la pagina anterior</a>";
 
 ?>
